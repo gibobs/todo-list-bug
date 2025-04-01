288900f159156f61b346c0cc31c176ef80c04ea9
danieldaga <daniel.garcia@tubuntu.tech>
2025-04-01 17:02:17 +0200

    middleware auth

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
 
+    @UseGuards(AuthGuard)
     @Get('')
     async listTasks() {
         return this.tasksService.listTasks();
     }
 
+    @UseGuards(AuthGuard)
     @Get('/:id')
     async getTask(@Param('id') id: string) {
         return this.tasksService.getTask(id);
     }
 
+    @UseGuards(AuthGuard)
     @Post('/edit')
     async editTask(@Body() body) {
         return this.tasksService.editTask(body);
